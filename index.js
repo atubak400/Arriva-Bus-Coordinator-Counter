8d721d1ec7102a1127c12637b33c53d16797e63e
Kingsley <atubak40@gmail.com>
2023-06-11 08:21:13 +0100

    added the total feature and incremented by 5 instead of 1 because 1 is too slow

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let total = 0
 
 function increment() {
-    count += 1
+    count += 5
     countEl.textContent = count
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    total += count;
+    totalEl.textContent = "Total: " + total;
+    countEl.textContent = 0;
+    count = 0;
 }
 
 
